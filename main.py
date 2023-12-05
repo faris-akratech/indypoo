739204e6a7e945f9e1f8af4e60b62ac2a88797da
faris-akratech <faris@akratech.in>
2023-12-05 14:26:09 +0530

    nomenclature changes

--- main.py
+++ main.py
@@ -4,30 +4,25 @@ import json
 from indy import pool
 from indy.error import ErrorCode, IndyError
 
-async def run(): 
+async def run():
     print("Indy demo program")
 
-    # print("STEP 1")
-    # pool = {
-    #     "name": "pool1"
-    # }
-
-    # print("Open pool ledger: {}".format(pool ['name']))
-    # pool ['genesis txn path'] = "pool1.txn"
-    # pool ['config'] = json.dumps({"genesis_txn": str(pool ['genesis txn path'])})
-
-    # print(pool)
+    pool_config = {
+        "name": "pool1",
+        "genesis_txn_path": "pool1.txn",
+        "config": json.dumps({"genesis_txn": "pool1.txn"})
+    }
 
     # connect to pool
     await pool.set_protocol_version(2)
-    try: 
-        await pool.create_pool_ledger_config(pool ['name'], pool ['config'])
+    try:
+        await pool.create_pool_ledger_config(pool_config['name'], pool_config['config'])
     except IndyError as ex:
         if ex.error_code == ErrorCode.PoolLedgerConfigAlreadyExistsError:
             pass
-    pool ['handle'] = await pool.open_pool_ledger(pool ['name'], None)
+    pool_handle = await pool.open_pool_ledger(pool_config['name'], None)
 
-    print(pool ['handle'])
+    print(pool_handle)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
