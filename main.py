cfac4a6359e59266df40a98d26237c0422c1dac6
faris-akratech <faris@akratech.in>
2023-12-05 14:24:22 +0530

    modified pool

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ async def run():
     # connect to pool
     await pool.set_protocol_version(2)
     try: 
-        await pool.create_pool_ledger_config(pool_['name'], pool_['config'])
+        await pool.create_pool_ledger_config(pool ['name'], pool ['config'])
     except IndyError as ex:
         if ex.error_code == ErrorCode.PoolLedgerConfigAlreadyExistsError:
             pass
-    pool_['handle'] = await pool.open_pool_ledger(pool_['name'], None)
+    pool ['handle'] = await pool.open_pool_ledger(pool ['name'], None)
 
-    print(pool_['handle'])
+    print(pool ['handle'])
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
